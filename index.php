461e780aa582a8e1ee8ad3ca725cd81e78db4a40
Christian-Goran <christiangoran4@gmail.com>
2025-02-04 23:42:50 -0500

    remise exercice 1

--- index.php
+++ index.php
@@ -9,7 +9,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>voyage</title>
   <link rel="stylesheet" href="style.css">
-  <?php wp_head(); ?>
 
 </head>
 <body>
