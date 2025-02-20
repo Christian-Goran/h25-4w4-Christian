e8486780e917a6bec3ebc2e2add0c53f2ac59d28
Christian-Goran <christiangoran4@gmail.com>
2025-02-20 09:39:24 -0500

    Création branche TP1

--- functions.php
+++ functions.php
@@ -2,7 +2,27 @@
 /**
  *   functions.php
  */
+
+
+ function mon_theme_supports() {
+    add_theme_support('post-thumbnails');
+    add_theme_support('title-tag');
+    add_theme_support('menus');
+    add_theme_support('custom-logo', array(
+        'height'      => 100,
+        'width'       => 400,
+        'flex-height' => true,
+        'flex-width'  => true,
+    ));
+}
+add_action( 'after_setup_theme', 'mon_theme_supports' );
+
 ?>
+
+
+
+
+
 <?php
 function ajouter_css_dans_header() {
 
@@ -21,4 +41,13 @@ function ajouter_css_dans_header() {
 
 }
 add_action('wp_enqueue_scripts', 'ajouter_css_dans_header');
+
+function modifie_requete_principal( $query ) {
+    if ( $query->is_home() && $query->is_main_query() && ! is_admin() ) {
+      $query->set( 'category_name', 'populaire' );
+      $query->set( 'orderby', 'title' );
+      $query->set( 'order', 'ASC' );
+      }
+     }
+     add_action( 'pre_get_posts', 'modifie_requete_principal' );
 ?>
