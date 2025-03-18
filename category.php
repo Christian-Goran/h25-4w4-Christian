d63a7a695405f0c541c0a970da1c68c7c5bf8d34
Christian-Goran <christiangoran4@gmail.com>
2025-03-17 23:00:03 -0400

    mise à jour de category.php ainsi que de carte.php

--- category.php
+++ category.php
@@ -1,14 +1,16 @@
-<?php get_header(); ?>
-    <h1><?php single_cat_title();?></h1>
-    <p><?php echo category_description(); ?></p>
-    <section class="populaire">
-        <div class="global">
-        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                <?php get_template_part( 'gabarits/carte' ); ?>
-            <?php endwhile; endif; ?>
-        </div>
+<?php /**
+ *  category.php qui sera utiliser lorsqu'on affiche une catégorie avec les boutons du menu
+ */
+?>
+
+<?php get_header() ?>
+
+    <section class="searched__populaire">
+
+            <?php get_template_part("gabarit/carte"); ?>
+
     </section>
-    <?php get_footer(); ?>
-   
+    
+<?php get_footer(); ?>
 </body>
 </html>
--- gabarit/carte.php
+++ gabarit/carte.php
@@ -1,23 +1,39 @@
-<?php
-/**
- * Gabarit permettant d'afficher une carte
-*/
-?>
-<article class="carte carte--grande">
-                <figure class="carte__image">
-                    <img src="images/img1.jpg" alt="Image de voyage">
-                </figure>
-                <div class="carte__contenu">
-                    <?php
-                    if (has_post_thumbnail()) {
-                        // permet d'afficher la petite image associé à l'article (image mise en avant)
-                        the_post_thumbnail('thumbnail'); }
-                    ?>
-                    <h2 class="carte__titre"><?php the_title(); ?></h2>
-                    <p class="carte__description"><?php echo wp_trim_words(get_the_excerpt(), 20, "...") ; ?></p>
-                    <a class="carte__bouton carte__bouton--actif"  href="<?php the_permalink(); ?>">Suite</a>
-                        <?php the_category() ?>
-                    <p>Température minimum&nbsp;<?php  echo the_field ('temperature_minimum'); ?>&#xB0;C</p>
-                    <p>Température maximum&nbsp;<?php echo the_field ('temperature_maximum'); ?>&#xB0;C</p>
-                </div>
-</article>
+<div class="boite__flex global">
+    
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    
+    <?php if (in_category('galerie')) {
+      
+      the_content();
+    
+    } else {?>
+
+    <article class="carte carte--grande">
+      
+        <div class="carte__contenu">
+            <?php
+            if (has_post_thumbnail()) {
+              
+                the_post_thumbnail('thumbnail'); }
+            ?>
+            <div class="carte__infos">
+
+              <a class="carte__titre" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+
+              <div class="carte__description">
+
+                <p><?php echo wp_trim_words(get_the_excerpt(), 20, "...")?></p>
+
+                <p>Température minimale: <?php echo the_field("temperature_minimale"); ?> °C</p>
+                <p>Température maximale: <?php echo the_field("temperature_maximale"); ?> °C</p>
+              </div>
+              <div class="carte__categorie">
+                <?php the_category(); ?>
+              </div>
+            </div>
+        </div>
+    </article>
+
+    <?php } ?>
+    <?php endwhile; endif; ?>
+</div>
