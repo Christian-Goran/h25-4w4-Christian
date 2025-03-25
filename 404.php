53606a84b83065507d3d040eb695aac442e6e7ed
Christian-Goran <christiangoran4@gmail.com>
2025-03-25 09:59:19 -0400

    Création du fichier pour une errreur 404

---
+++ 404.php
@@ -0,0 +1,14 @@
+<h2>Erreur 404</h2>
+<?php get_header(); ?>
+    
+  
+    <div class="searched__populaire">
+        <?php get_template_part('gabarit/carte'); ?>
+
+    </div>
+
+<?php get_footer(); ?>
+
+</body>
+
+</html>
