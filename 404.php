bb3440c04a9b8a6dcf38bac50dffd4dbd77b3d54
Christian-Goran <christiangoran4@gmail.com>
2025-03-25 11:00:20 -0400

    création du style de la page

--- 404.php
+++ 404.php
@@ -7,10 +7,7 @@
         <a href="<?php echo home_url(); ?>" class="btn-retour">Retour à l'accueil</a>
     </section>
 
-    <div class="searched__populaire">
-        <h2>Pages populaires</h2>
-        <?php get_template_part('gabarit/carte'); ?>
-    </div>
+ 
 </main>
 
 <?php get_footer(); ?>
