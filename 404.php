7a559293a031378a7fff130ca0b82ccbb3242014
Christian-Goran <christiangoran4@gmail.com>
2025-03-25 10:19:43 -0400

    Première version de la page avec bouton de retour à l'acceuil

--- 404.php
+++ 404.php
@@ -1,14 +1,16 @@
-<h2>Erreur 404</h2>
 <?php get_header(); ?>
-    
-  
-    <div class="searched__populaire">
-        <?php get_template_part('gabarit/carte'); ?>
 
+<main class="erreur-404">
+    <section class="contenu">
+        <h1>Erreur 404</h1>
+        <p>Désolé, la page que vous recherchez est introuvable.</p>
+        <a href="<?php echo home_url(); ?>" class="btn-retour">Retour à l'accueil</a>
+    </section>
+
+    <div class="searched__populaire">
+        <h2>Pages populaires</h2>
+        <?php get_template_part('gabarit/carte'); ?>
     </div>
+</main>
 
 <?php get_footer(); ?>
-
-</body>
-
-</html>
