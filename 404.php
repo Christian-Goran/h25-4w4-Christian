8bf1da181ac3e61e89825975eb4e86ab45263f08
Christian-Goran <christiangoran4@gmail.com>
2025-04-01 11:27:38 -0400

    modification de certaines variables + modification du css

--- 404.php
+++ 404.php
@@ -5,60 +5,61 @@
 ?>
 
 <?php 
-  $image_erreur_404 = get_theme_mod('erreur_404_image_fond', 'Image de fond par défaut');
-  $couleur_erreur_404 = get_theme_mod('erreur_404_couleur_fond', 'Couleur par défaut');
+  $image_erreur_404 = get_theme_mod('erreur_image_fond', '');
+  $couleur_erreur_404 = get_theme_mod('erreur_couleur_fond', '#ffffff');
 
-  $titre_erreur_404 = get_theme_mod('erreur_404_titre', 'Oops, vous avez échoué sur l\'île 404!');
-  $message_erreur_404 = get_theme_mod('erreur_404_message', 'Désolé, la page que vous recherchez n’existe pas ou a été déplacée.');
+  $titre_erreur_404 = get_theme_mod('erreur_titre', 'Default Title');
+  $message_erreur_404 = get_theme_mod('erreur_message', 'Default Title');
 
-  $lien_retour_accueil = get_theme_mod('erreur_404_lien_accueil', 'Retour à la page principale');
+  $lien_retour_accueil = get_theme_mod('erreur_lien_accueil', home_url());
 
-  $suggestion_article_1 = get_theme_mod('erreur_404_nom_article_1', 'Article recommandé 1');
-  $lien_suggestion_1 = get_theme_mod('erreur_404_lien_article_1', '#');
+  $suggestion_article_1 = get_theme_mod('erreur_404_nom_article_1', 'Default Title');
+  $lien_suggestion_1 = get_theme_mod('erreur_404_lien_article_1', 'Default Title');
 
-  $suggestion_article_2 = get_theme_mod('erreur_404_nom_article_2', 'Article recommandé 2');
-  $lien_suggestion_2 = get_theme_mod('erreur_404_lien_article_2', '#');
+  $suggestion_article_2 = get_theme_mod('erreur_404_nom_article_2', 'Default Title');
+  $lien_suggestion_2 = get_theme_mod('erreur_404_lien_article_2', 'Default Title');
 
-  $suggestion_article_3 = get_theme_mod('erreur_404_nom_article_3', 'Article recommandé 3');
-  $lien_suggestion_3 = get_theme_mod('erreur_404_lien_article_3', '#');
+  $suggestion_article_3 = get_theme_mod('erreur_404_nom_article_3', 'Default Title');
+  $lien_suggestion_3 = get_theme_mod('erreur_404_lien_article_3', 'Default Title');
 
-  $suggestion_article_4 = get_theme_mod('erreur_404_nom_article_4', 'Article recommandé 4');
-  $lien_suggestion_4 = get_theme_mod('erreur_404_lien_article_4', '#');
+  $suggestion_article_4 = get_theme_mod('erreur_404_nom_article_4', 'Default Title');
+  $lien_suggestion_4 = get_theme_mod('erreur_404_lien_article_4', 'Default Title');
 ?>
 
 <?php get_header(); ?>
     
-  <section class="erreur-404" style="background-image: url(<?php echo($image_erreur_404) ?>)">
-    <h1 class="erreur-404__titre"><?php echo($titre_erreur_404)?></h1>
-    <h3 class="erreur-404__message"><?php echo($message_erreur_404)?></h3>
+  <section class="erreur-404" style="background-image: url(<?php echo esc_url($image_erreur_404); ?>); background-color: <?php echo esc_attr($couleur_erreur_404); ?>;">
+    <h1 class="erreur-404__titre"><?php echo esc_html($titre_erreur_404); ?></h1>
+    <h3 class="erreur-404__message"><?php echo esc_html($message_erreur_404); ?></h3>
 
-    <a href="<?php echo($lien_retour_accueil)?>" class="erreur-404__bouton no-underline">Retourner à la page d'accueil</a>
+    <a href="<?php echo esc_url($lien_retour_accueil); ?>" class="erreur-404__bouton no-underline">Retourner à la page d'accueil</a>
 
     <div class="erreur-404__suggestions">
-      <a href="<?php echo($lien_suggestion_1)?>" class="" 
-      style="background-color: <?php echo($couleur_erreur_404) ?>;">
-        <?php echo($suggestion_article_1)?>
-      </a>
-      <a href="<?php echo($lien_suggestion_2)?>" class="" 
-      style="background-color: <?php echo($couleur_erreur_404) ?>;">
-        <?php echo($suggestion_article_2)?>
-      </a>
-      <a href="<?php echo($lien_suggestion_3)?>" class="" 
-      style="background-color: <?php echo($couleur_erreur_404) ?>;">
-        <?php echo($suggestion_article_3)?>
-      </a>
-      <a href="<?php echo($lien_suggestion_4)?>" class="" 
-      style="background-color: <?php echo($couleur_erreur_404) ?>;">
-        <?php echo($suggestion_article_4)?>
-      </a>
+      <?php if ($suggestion_article_1) : ?>
+        <a href="<?php echo esc_url($lien_suggestion_1); ?>" style="background-color: <?php echo esc_attr($couleur_erreur_404); ?>;">
+          <?php echo esc_html($suggestion_article_1); ?>
+        </a>
+      <?php endif; ?>
+      <?php if ($suggestion_article_2) : ?>
+        <a href="<?php echo esc_url($lien_suggestion_2); ?>" style="background-color: <?php echo esc_attr($couleur_erreur_404); ?>;">
+          <?php echo esc_html($suggestion_article_2); ?>
+        </a>
+      <?php endif; ?>
+      <?php if ($suggestion_article_3) : ?>
+        <a href="<?php echo esc_url($lien_suggestion_3); ?>" style="background-color: <?php echo esc_attr($couleur_erreur_404); ?>;">
+          <?php echo esc_html($suggestion_article_3); ?>
+        </a>
+      <?php endif; ?>
+      <?php if ($suggestion_article_4) : ?>
+        <a href="<?php echo esc_url($lien_suggestion_4); ?>" style="background-color: <?php echo esc_attr($couleur_erreur_404); ?>;">
+          <?php echo esc_html($suggestion_article_4); ?>
+        </a>
+      <?php endif; ?>
     </div>
 
     <div class="erreur-404__recherche">
-        <?php get_search_form() ?>
+        <?php get_search_form(); ?>
     </div>
   </section>
 
-  <?php get_footer(); ?>
-
-</body>
-</html>
+<?php get_footer(); ?>
--- functions/customizer.php
+++ functions/customizer.php
@@ -105,7 +105,70 @@ function theme_31w_customize_register($wp_customize) {
     'section' => 'footer_section',
     'type' => 'text_area',
   ));
-}
-add_action('customize_register', 'theme_31w_customize_register');
 
-?>
+
+
+
+
+
+  // Section principale 404
+  $wp_customize->add_section('erreur_section', array(
+      'title'    => __('Page 404', 'theme_31w'),
+      'priority' => 30,
+  ));
+
+  // Titre et message d'erreur
+  $wp_customize->add_setting('erreur_titre', array(
+      'default'           => __('Oops! Page introuvable', 'theme_31w'),
+      'sanitize_callback' => 'sanitize_text_field',
+  ));
+  $wp_customize->add_control('erreur_titre', array(
+      'label'   => __('Titre d’erreur', 'theme_31w'),
+      'section' => 'erreur_section',
+      'type'    => 'text',
+  ));
+
+  $wp_customize->add_setting('erreur_message', array(
+      'default'           => __('Désolé, la page que vous recherchez est introuvable.', 'theme_31w'),
+      'sanitize_callback' => 'sanitize_text_field',
+  ));
+  $wp_customize->add_control('erreur_message', array(
+      'label'   => __('Message d’erreur', 'theme_31w'),
+      'section' => 'erreur_section',
+      'type'    => 'textarea',
+  ));
+
+  // Lien vers l'accueil
+  $wp_customize->add_setting('erreur_lien_accueil', array(
+      'default'           => home_url(),
+      'sanitize_callback' => 'esc_url_raw',
+  ));
+  $wp_customize->add_control('erreur_lien_accueil', array(
+      'label'   => __('Lien vers l’accueil', 'theme_31w'),
+      'section' => 'erreur_section',
+      'type'    => 'url',
+  ));
+
+  // Image de fond
+  $wp_customize->add_setting('erreur_image_fond', array(
+      'default'           => '',
+      'sanitize_callback' => 'esc_url_raw',
+  ));
+  $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'erreur_image_fond', array(
+      'label'   => __('Image de fond', 'theme_31w'),
+      'section' => 'erreur_section',
+  )));
+
+  // Couleur de fond
+  $wp_customize->add_setting('erreur_couleur_fond', array(
+      'default'           => '#ffffff',
+      'sanitize_callback' => 'sanitize_hex_color',
+  ));
+  $wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'erreur_couleur_fond', array(
+      'label'   => __('Couleur de fond', 'theme_31w'),
+      'section' => 'erreur_section',
+  )));
+
+
+} 
+add_action('customize_register', 'theme_31w_customize_register');?>
